8f07066a3553349c13e501b8161f334f3c70353b
nichanat1102 <nichanat.sa@ku.th>
2025-12-22 14:42:29 +0700

    add the new test case

--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -4,6 +4,10 @@ describe('Calculator', () => {
     test('adds 2 + 3 to equal 5', () => {
         expect(add(2, 3)).toBe(5);
     });
+    
+    test('adds -2 + -3 to equal 5', () => {
+        expect(add(-2, -3)).toBe(5);
+    });
 
     test('subtracts 5 - 3 to equal 2', () => {
         expect(subtract(5, 3)).toBe(2);
